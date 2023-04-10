a533b478e48354a4582511ea395a209cbdb6d92b
Rainfog <muhammadzhafranbahij@gmail.com>
2023-04-11 04:29:59 +0700

    Step 1: Input data; Step 2: Transform data to matrix W

--- input_processing.py
+++ input_processing.py
@@ -2,30 +2,32 @@ import data_from_database as dfd
 import nltk
 import numpy
 import pandas
+import re
 
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
 # Mengambil data dari database
-dataset = dfd.get_document_and_word()
+dataset_document_word, dataset_tag_document = dfd.get_document_and_word()
+# dataset_tag_document = dfd.get_document_and_tag()
 
 # Tempat untuk menghitung banyaknya word dalam suatu dokumen
 dataframe_document_word = []
-
-# Tempat untuk menghitung banyaknya tag dalam suatu dokumen
-dataframe_document_tag = []
 
 def wordProcessing(content_article):
     """
     Fungsi untuk menghitung 'word' dalam suatu artikel
     """
-    tokens = word_tokenize(content_article.lower())
-    english_stopwords = stopwords.words('english')
+    tokens = word_tokenize(re.sub('[^ 0-9a-z]+', ' ', content_article.lower())) # Menghilangkan tanda baca   
+    english_stopwords = stopwords.words('english') # Menghilangkan stop words
+    english_stopwords.extend(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'])
     tokens_wo_stopwords = [t for t in tokens if t not in english_stopwords]
 
+    # Menghitung banyaknya tokens
     freq = nltk.FreqDist(tokens_wo_stopwords)
     word_document_dictionary = {}
 
+    # Menampung token tersebut dalam format dictionary
     for word, count in freq.most_common(999999):
         word_document_dictionary.update({word: count})
     
@@ -41,38 +43,37 @@ def documentWordProcessing(content_article, title_article):
     )
     return datafr
 
-def tagProcessing(tag_article):
-    """
-    Fungsi untuk menghitung 'tag' dalam suatu artikel 
-    """
-    tag_article = tag_article.replace('\t', '')
-    tokens = nltk.regexp_tokenize(tag_article.lower(), '\n', gaps=True)
-
-    freq = nltk.FreqDist(tokens)
-    tag_document_dictionary = {}
-
-    for tag, count in freq.most_common(999999):
-        tag_document_dictionary.update({tag: count})
+for data in dataset_document_word:
+    dataframe_document_word.append(documentWordProcessing(data[1], data[0]))
     
-    return tag_document_dictionary
-
-def documentTagProcessing(tag_article, title_article):
-    tag_document_dictionary = tagProcessing(tag_article)
-    datafr = pandas.DataFrame(tag_document_dictionary,
-        index=[title_article]
-    )
-    return datafr
-
-for data in dataset:
-    dataframe_document_word.append(documentWordProcessing(data[2], data[0]))
-    dataframe_document_tag.append(documentTagProcessing(data[1], data[0]))
+def document_tag_processing(dataset_tag_document):  
+  title_before = dataset_tag_document[0][1]
+  
+  tag_dictionary = {}
+  # Tempat untuk menghitung banyaknya tag dalam suatu dokumen
+  dataframe_document_tag = []
+  
+  for data in dataset_tag_document:
+    # Jika judul dokumen berbeda dengan row sebelumnya
+    if title_before != data[1]:
+      datafr = pandas.DataFrame(tag_dictionary,
+          index=[title_before]
+      )
+      dataframe_document_tag.append(datafr)
+      title_before = data[1]
+      tag_dictionary.clear()
+    
+    tag_dictionary.update({data[0]: 1})
+  
+  return dataframe_document_tag
+    
+dataframe_document_tag_join = pandas.concat(document_tag_processing(dataset_tag_document))
+dataframe_document_tag_join = dataframe_document_tag_join.fillna(0)
+print(dataframe_document_tag_join)
 
 dataframe_document_word_join = pandas.concat(dataframe_document_word)
 dataframe_document_word_join = dataframe_document_word_join.fillna(0)
-dataframe_document_tag_join = pandas.concat(dataframe_document_tag)
-dataframe_document_tag_join = dataframe_document_tag_join.fillna(0)
-
-# def dataframeToMatrix(document_word, document_tag):
+print(dataframe_document_word_join)
 
 def matrixABtoW(A, B):
     """
@@ -108,5 +109,6 @@ def matrixABtoW(A, B):
 # print(dataframe_document_word_join.to_numpy())
 # print(dataframe_document_tag_join.to_numpy())
 
-matrix_w = matrixABtoW(dataframe_document_tag_join.to_numpy().transpose(), dataframe_document_word_join.to_numpy())
-print(matrix_w)
+# matrix_w = matrixABtoW(dataframe_document_tag_join.to_numpy().transpose(), dataframe_document_word_join.to_numpy())
+# print(matrix_w)
+
