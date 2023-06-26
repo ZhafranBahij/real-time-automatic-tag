Fix bottleneck, alpha, and vector normalization

low_rank_approximation_matrix.py
@@ -1,5 +1,24 @@
 import numpy as np
 
+W = [
+[0 , 0 , 1 , 1 , 1 , 0 , 0 , 0 , 0] ,
+[0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0] ,
+[1 , 0 , 0 , 0 , 0 , 1 , 1 , 1 , 0] ,
+[1 , 0 , 0 , 0 , 0 , 0 , 1 , 1 , 0] ,
+[1 , 1 , 0 , 0 , 0 , 0 , 0 , 1 , 1] ,
+[0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0] ,
+[0 , 0 , 1 , 1 , 0 , 0 , 0 , 0 , 0] ,
+[0 , 0 , 1 , 1 , 1 , 0 , 0 , 0 , 0] ,
+[0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0] ,
+]
+
+matrix_graph = np.array(W)
+
+# def normalization_vector(vector):
+#   vector_power = [np.power(x, 2) for x in vector]
+#   vector_normalize = vector / np.sqrt(np.sum(vector_power))
+#   return vector_normalize
+
 def lanczos_iteration(A, one_b = 1):
   """
   Melakukan Lanczos iteration dengan membuat matriks Q dan T
@@ -28,7 +47,8 @@ def lanczos_iteration(A, one_b = 1):
   else:
     b = np.ones((row, 1)) 
     
-
+  # Matriks q_now adalah matriks yg telah normalisasi
+  # panjang q_now = 1
   q_now = b / np.linalg.norm(b)
   
   Q[:, 0] = q_now.transpose()
@@ -36,6 +56,7 @@ def lanczos_iteration(A, one_b = 1):
   for i in range(1, 3):
     v = A.dot(q_now)
     alpha = q_now.transpose().dot(v)
+    alpha = alpha[0][0] # Membuat alpha agar menjadi skalar
     v = v - beta*q_before - alpha*q_now
     beta = np.linalg.norm(v)
     q_before = q_now
@@ -65,3 +86,7 @@ def low_rank_approximation_matrix(Q, T):
   """
   
   return Q.dot(T).dot(Q.transpose())
+
+# Q, T = lanczos_iteration(matrix_graph)
+# w_hat = low_rank_approximation_matrix(Q, T)
+# print(w_hat)

main.py
@@ -6,27 +6,24 @@ import normalized_laplacian as nl
 import low_rank_approximation_matrix as lram
 
 # Source library lain
-import datetime
+import the_moment as tm
+import numpy as np
 
-now = datetime.datetime.now()
-print("Inisialisasi :",now)
+tm.this_moment("Menjalankan Algoritma :")
 
 # Mengambil data dari database
 dataset_document = dfd.get_data()
-now = datetime.datetime.now()
-print("Mengambil dataset :",now)
+tm.this_moment("Mengambil dataset :")
 # print(dataset_document)
 
 # Memproses dataset menjadi matrix
 matrix_tag_document, matrix_document_word, title_id_document = ip.document_processing(dataset_document)
-now = datetime.datetime.now()
-print("Dataset ke Matrix :",now)
+tm.this_moment("dataset ke matrix :")
 # print(matrix_tag_document)
 # print(matrix_document_word)
 
 matrix_w = mp.matrixABtoW(matrix_tag_document, matrix_document_word)
-now = datetime.datetime.now()
-print("Matrix A & B menjadi W :",now)
+tm.this_moment("matrix A & B menjadi W :")
 # print(matrix_w)
 
 # matrix_d = nl.diagonal_matrix(matrix_w)
@@ -37,15 +34,13 @@ print("Matrix A & B menjadi W :",now)
 
 matrix_Q, matrix_T = lram.lanczos_iteration(matrix_w, 1)
 matrix_W_hat = lram.low_rank_approximation_matrix(matrix_Q, matrix_T)
-now = datetime.datetime.now()
-print("Low Rank Approximation :",now)
+tm.this_moment("Low Rank Approximation :")
 # print(matrix_W_hat)
 
 matrix_Q_2, matrix_T_2 = lram.lanczos_iteration(matrix_w, 0)
 matrix_W_hat_2 = lram.low_rank_approximation_matrix(matrix_Q_2, matrix_T_2)
-now = datetime.datetime.now()
-print("Low Rank Approximation :",now)
+tm.this_moment("Low Rank Approximation :")
 # print(matrix_W_hat_2)
 
-matrix_W_hat_diff = matrix_W_hat - matrix_W_hat_2
+matrix_W_hat_diff = np.absolute(matrix_W_hat - matrix_W_hat_2)
 print(matrix_W_hat_diff)